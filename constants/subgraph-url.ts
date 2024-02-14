959d4ba8359d2d6f1b41800400255aaaa5440199
graykode <nlkey2022@gmail.com>
2024-02-14 19:38:37 +0900

    fix: use change graph url

--- constants/subgraph-url.ts
+++ constants/subgraph-url.ts
@@ -4,5 +4,5 @@ export const SUBGRAPH_URL: {
   [chain in CHAIN_IDS]: string
 } = {
   [CHAIN_IDS.BERA_ARTIO]:
-    'https://dev-bera.clober-api.com/subgraphs/name/core-v1-subgraph',
+    'https://api.goldsky.com/api/public/project_clsljw95chutg01w45cio46j0/subgraphs/core-v1-subgraph/v1.0.0/gn',
 }
